89d123c4ec392174d0c364108ae9d575e4ba94b7
alliL <allison.lee124@gmail.com>
2018-07-02 00:45:33 -1000

    outputs a .csv file of global clinician information

--- script.py
+++ script.py
@@ -4,6 +4,10 @@ import time
 import numpy as np
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from datetime import datetime
+
+# to time how long the script runs
+start_time = datetime.now()
 
 # Accepts an array and a string
 # Inserts value into the array. If the value is NoneType or an empty string,
@@ -19,7 +23,7 @@ def insert_value(clinician, value):
 attributes = ['Clinician URL', 'Image URL', 'Category', 'First Name',
               'Last Name', 'Title', 'Office Name', 'Address', 'City',
               'State', 'Zip/Postal Code','Country', 'Phone', 'Fax', 'Email',
-              'Website', 'Alternate Office State',
+              'Website', 'Alternate Office Website', 'Alternate Office State',
               'Alternate Office Country', 'Languages Spoken',
               'Type of Facility',
               'Does Your Facility Offer Both LSVT BIG & LSVT LOUD',
@@ -31,6 +35,7 @@ clinicians = np.array([attributes])
 
 driver = webdriver.Chrome()
 
+# open the browser to the given link
 driver.get('https://www.lsvtglobal.com/clinicians')
 
 # Choose the 'LSVT BIG Physical Therapists or Occupational Therapists' option
@@ -42,12 +47,12 @@ driver.find_element_by_xpath("//*[@id='clinician_terms_checkbox']").click()
 # Submit query
 driver.find_element_by_xpath("//*[@id='uniform-undefined']//span//input[@type='submit' and @value='Search']").click()
 
-time.sleep(3)
+time.sleep(5)
 
 # lazy-load all clinicians (8641)
-#element = driver.find_element_by_id("advanced_search_wrap")
-#driver.execute_script("arguments[0].scrollIntoView();", element)
-#time.sleep(500)
+element = driver.find_element_by_id("advanced_search_wrap")
+driver.execute_script("arguments[0].scrollIntoView();", element)
+time.sleep(500)
 
 # Selenium hands the page source to Beautiful Soup
 all_soup = BeautifulSoup(driver.page_source, 'lxml')
@@ -56,6 +61,7 @@ all_soup = BeautifulSoup(driver.page_source, 'lxml')
 clinician_links = []
 for clinician_link in all_soup.find_all('li', class_=['one', 'two', 'three']):
     clinician_links.append(clinician_link.h2.a['href'])
+print('Number of Clinicians: ', len(clinician_links))
 
 # scrape info about each clinician
 for link in clinician_links:
@@ -72,17 +78,25 @@ for link in clinician_links:
     clinician = insert_value(clinician, link)
 
     # get the clinician's image url and insert it into the clinician's array
-    image_url = single_soup.find('img', class_ = 'flt-right-small')
+    image_url = single_soup.find('img', class_ = 'flt-right small')
+    if image_url != None:
+        image_url = image_url['src']
     clinician = insert_value(clinician, image_url)
 
     # get the clinician's category and insert it into the clinician's array
-    category = single_soup.find('div', class_='cms-main').p.text
+    category = single_soup.find('div', class_='cms-main')
+    if category != None:
+        category = category.p.text
     clinician = insert_value(clinician, category)
 
     # for each attribute in the list of attributes, collect the information
     # corresponding to the attribute
     for attribute in attributes[3:]:
-        pattern = re.compile('(?<=%s</th><td>)(.*?)(?=</td>)' % attribute)
+        pattern = re.compile('(?<=%s</th><td>)(.*?)(?=</td>)'%attribute)
+        if attribute == "Website":
+            pattern = re.compile('(?<=Website</th><td><a href=")(.*?)(?=" target)')
+        elif attribute == "Alternate Office Website":
+            pattern = re.compile('(?<=Office Website</th><td><a href=")(.*?)(?=" target)')
         match = pattern.search(str(single_soup))
         if match:
             value = match.group()
@@ -90,11 +104,21 @@ for link in clinician_links:
             value = None
         clinician = insert_value(clinician, value)
 
-    print(clinician)
-    print(len(attributes), len(clinician))
     clinicians = np.vstack((clinicians, clinician))
 
-print("All Clinicians: ", clinicians)
-
+# close the browser
 driver.quit()
+
+# open a .csv file
+outfile = open('LSVT_BIG.csv','w', newline='')
+writer = csv.writer(outfile)
+
+# insert rows per clinician
+for clinician in clinicians:
+    writer.writerow(clinician)
+
+# close the .csv file
+outfile.close()
+
+print(datetime.now() - start_time)
 print("DONE")
